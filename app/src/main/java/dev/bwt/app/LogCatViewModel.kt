d6198f5911b3be34d413c7ae839ec5d97cd8f111
Nadav Ivgi <nadav@shesek.info>
2020-12-31 02:41:11 +0200

    Grab the last 100 logcat messages more reliably

--- app/src/main/java/dev/bwt/app/LogCatViewModel.kt
+++ app/src/main/java/dev/bwt/app/LogCatViewModel.kt
@@ -6,18 +6,20 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 
 var CMD =
-    "logcat -b main -v tag {OPT} '*:V' bwt-daemon:D bwt-main:D| grep --line-buffered -E '^[A-Z]/(bwt|bitcoin)'"
+    "logcat -b main -v tag {OPT} '*:V' bwt-daemon:D bwt-main:D | grep --line-buffered -E '^[A-Z]/(bwt|bitcoin)'"
 
 class LogCatViewModel : ViewModel() {
     fun logCatOutput() = liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
         // Read the last 100 messages and pass them in one go, then stream new messages one at a time.
-        // This is done to avoid a lag when appending the messages to the TextView.
+        // This is done to avoid a lag when appending messages to the TextView one-by-one.
 
-        sh(CMD.replace("{OPT}", "-T 100 -d"))
+        sh(CMD.replace("{OPT}", "-T 500 -d"))
             .inputStream
             .bufferedReader()
             .useLines { lines ->
-                emit(lines.joinToString("\n"))
+                // Grab the last 500 messages, then grep for relevant ones, then take the last 100 messages
+                // Needed because there's a high percentage of non-relevant messages
+                emit(lines.toList().takeLast(100).joinToString("\n"))
             }
 
         sh(CMD.replace("{OPT}", "-T 1"))
